8ddd8e71664f302371664aa4c5a9215c5c3e4e92
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-03-29 14:43:04 +0200

    started with the login components and home component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+
+  { path: 'secure', component: SecureComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    SecureComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
