d1eb51b9bdb6771000673f85889357133d0ae147
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-04-01 17:10:48 +0200

    start with registration form

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group( {
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+
+    })
   }
 
 }
